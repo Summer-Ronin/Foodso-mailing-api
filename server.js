84e960a7b515b89229a573947c5287f03a10b462
NhutNguyen236 <nhutnguyenf330@gmail.com>
2021-08-09 12:47:49 +0700

    :fire: init

---
+++ server.js
@@ -0,0 +1,69 @@
+/**
+ *
+ */
+var express = require("express");
+var nodemailer = require("nodemailer");
+const hbs = require("nodemailer-handlebars");
+
+var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// sending email route
+app.post("/sendMail/:email", (req, res) => {
+	var client_email = req.params.email;
+	console.log(client_email);
+	var client_name = client_email.split("@")[0];
+
+	const transporter = nodemailer.createTransport({
+		service: "gmail",
+		auth: {
+            user: "YOUR_EMAIL",
+            pass: "YOUR_EMAIL_PASSWORD",
+		},
+	});
+
+	transporter.use(
+		"compile",
+		hbs({
+			viewEngine: {
+				defaultLayout: false,
+				extName: "express-handlebars",
+			},
+			viewPath: __dirname,
+		})
+	);
+
+	let mailOptionsClient = {
+		from: "sueh.qt.ueh@gmail.com",
+		to: client_email,
+		subject: "💓 FoodSo xin chân thành cảm ơn💓",
+		template: "mail_template",
+		context: {
+			name: client_name,
+		}, // send extra values to template
+	};
+
+	transporter.sendMail(mailOptionsClient, (err, data) => {
+		var msg = undefined;
+		if (err) {
+			console.log(err);
+			msg =
+				"We are facing some technical difficulties here, come back later 😥";
+			return res.send(msg);
+		}
+		msg = "OK";
+		res.send(msg);
+	});
+});
+
+// Handmade 404 page
+app.use((req, res) => {
+	res.status(404);
+	res.send("Đường dẫn hoặc phương thức không hợp lệ");
+});
+
+var server = app.listen(process.env.PORT || 3000, () => {
+	console.log("The server is now running at http://localhost:" + 3000);
+});
